7f546c6a4fc3bef16bba39c0bf1ad715dd32bd02
Vishwantiwari <tiwarivishwan5899@gmail.com>
2021-04-23 23:21:00 +0530

    Create Linked List is Palindrome or not.cpp

---
+++ Linked List is Palindrome or not.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+class Solution{
+  public:
+    //Function to check whether the list is palindrome.
+    bool isPalindrome(Node *head)
+    {stack<int>s;
+Node *h1=head;
+while(h1!=NULL)
+{
+s.push(h1->data);
+h1=h1->next;
+}
+while(head!=NULL)
+{
+if(head->data!=s.top())
+{
+return 0;
+}
+s.pop();
+head=head->next;
+}
+return 1;
+}
+};
+
+
+int main()
+{
+  int T,i,n,l,firstdata;
+    cin>>T;
+    while(T--)
+    {
+        
+        struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        // taking first data of LL
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        // taking remaining data of LL
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+    Solution obj;
+   	cout<<obj.isPalindrome(head)<<endl;
+    }
+    return 0;
+}
