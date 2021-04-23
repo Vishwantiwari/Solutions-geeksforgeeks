eb30357e5691768c2f543c219628dd2e79d772b3
Vishwantiwari <tiwarivishwan5899@gmail.com>
2021-04-23 22:52:08 +0530

    Create Reverse a Linked List.cpp

---
+++ Reverse a Linked List.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *head)
+    {struct Node *current,*next,*prev;
+current=head;
+prev=NULL;
+while(current!=NULL)
+{
+next=current->next;
+current->next=prev;
+prev=current;
+current=next;
+}
+head=prev;
+return head;
+}
+     
+    
+    
+};
+   
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+       printf("%d ", temp->data);
+       temp  = temp->next;
+    }
+}
+
+
+int main()
+{
+    int T,n,l,firstdata;
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        
+        for (int i=1; i<n; i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        head = ob. reverseList(head);
+        
+        printList(head);
+        cout << endl;
+    }
+    return 0;
+}
